3625162243f67b4972f4cf78d21b75be392b81be
Artyom <olsenaxxx@gmail.com>
2018-10-15 19:12:49 +0300

    Empty layout modification

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-app dark>
+  <v-app>
+    <v-layout class="matrix">
+      <canvas id="matr"></canvas>
+    </v-layout>
 <!--     {{ $t('test') }}
     <v-btn style="width:200px" v-on:click='changeLang'>{{$i18n.locale}}</v-btn> -->
     <nuxt />
   </v-app>
 </template>
 
-<!-- <script>
+<script>
+  import matrix from '@/assets/scripts/matrix'
   export default {
     methods: {
-      changeLang () {
-        this.$i18n.locale = (this.$i18n.locale === 'ru') ? 'en' : 'ru'
-      }
+      // changeLang () {
+      //   this.$i18n.locale = (this.$i18n.locale === 'ru') ? 'en' : 'ru'
+      // }
+    },
+
+    mounted () {
+      matrix()
     }
   }
-</script> -->
+</script>
+
+<style scoped>
+  .canvas#matr{
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    width: 100%;
+    margin: auto;
+  }
+
+  .matrix{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    z-index: 0;
+  }
+</style>
