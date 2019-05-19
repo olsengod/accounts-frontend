976cbb5d28fda77ab73f89b1f13c5dc6ec0884f1
Artyom <olsenaxxx@gmail.com>
2019-05-19 15:02:06 +0300

    Regex fix

--- config/http.js
+++ config/http.js
@@ -5,7 +5,7 @@ module.exports = {
   is_dev: {
     backendURL: 'https://accounts-dev.cereris.org',
     torlightBackendURL: 'https://torlight-app-stage.cereris.org',
-    regexTorlightBackendURL: '/https:\/\/torlight-app-stage.cereris.org\//i'
+    regexTorlightBackendURL: /https:\/\/torlight-app-stage.cereris.org\//i
     // torlightBackendURL: 'http://localhost:3000',
     // regexTorlightBackendURL: '/http:\/\/localhost:3000\//i'
   }
