d761d824010df4ae08bf6066d77bc9efae83fb29
Артём Оликов <olsenaxxx@gmail.com>
2019-05-20 16:14:46 +0300

    Temp

--- middleware/setQueryTokens.js
+++ middleware/setQueryTokens.js
@@ -3,7 +3,6 @@ import ls from 'local-storage'
 
 export default async function ({ query }) {
   if ('accessToken' in query && 'refreshToken' in query && 'expiresIn' in query) {
-    console.log('LOL')
     ls.set('cererisAccountAccessToken', query.accessToken)
     ls.set('cererisAccountRefreshToken', query.refreshToken)
     ls.set('cererisExpiresIn', query.expiresIn)
